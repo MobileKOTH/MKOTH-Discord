519ab8f64e1b6b7f957a405300071c764470635e
CHEN YU <scheduledx@gmail.com>
2017-10-15 00:27:54 +0800

    Discord Start

--- MKOTH Discord Bot/Program.cs
+++ MKOTH Discord Bot/Program.cs
@@ -3,14 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Discord;
+using Discord.WebSocket;
+using Discord.Net.Providers.WS4Net;
 
 namespace MKOTH_Discord_Bot
 {
     class Program
     {
-        static void Main(string[] args)
+        private DiscordSocketClient _client;
+
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
-            Console.ReadLine();
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                WebSocketProvider = WS4NetProvider.Instance,
+                LogLevel = LogSeverity.Info
+            });
+
+            _client.Log += Log;
+
+            string token = File.ReadAllText("token.txt"); // Remember to keep this private!
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
