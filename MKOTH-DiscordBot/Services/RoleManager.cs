3089fbdc08727be39561d59ca3cc5a9d88807976
Cerlancism <scheduledx@gmail.com>
2020-07-25 19:10:02 +0800

    Update RoleManager.cs

--- MKOTH-DiscordBot/Services/RoleManager.cs
+++ MKOTH-DiscordBot/Services/RoleManager.cs
@@ -58,7 +58,7 @@ namespace MKOTHDiscordBot.Services
         private async Task HandleSeriesUpdate()
         {
             Logger.Debug("Updating Member Roles", "Update");
-            // TODO: Now all users are not guaranteed to be complete, especially offline users.
+
             var members = await Guild.GetUsersAsync();
 
             if (members.Count != (Guild as SocketGuild).MemberCount)
